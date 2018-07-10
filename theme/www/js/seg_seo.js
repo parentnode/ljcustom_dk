b91d024f33e793d5ca736c104046a5918f15a304
Martin Kæstel Nielsen <martin@think.dk>
2018-07-10 23:26:43 +0200

    Merged

---
+++ theme/www/js/seg_seo.js
@@ -0,0 +1,12 @@
+/*
+parentNode, Copyright 2017, https://.parentnode.dk
+js-merged @ 2018-07-10 23:26:23
+*/
+
+/*seg_seo_include.js*/
+
+/*i-unsupported.js*/
+function unsupported() {
+	document.body.innerHTML = '<h1 style="text-align: center; margin: 20% 15% 15%; font-family: Arial; color: #333333">Your browser is NOT supported. It is more outdated than steam-engines, typewriters and VHS tapes, so stop acting surprised.</h1><p style="text-align: center; margin: 0 10%; font-family: Arial; color: #333333">or<br />(you changed your UserAgent)<br />(we are working on it)<br />(we made an error - <a href="mailto:info@parentnode.dk">let us know</a>)</p>';
+}
+window.onload = unsupported;
